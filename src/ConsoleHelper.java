42b3add54902a3deb8904ffa234a84c2a8b695e5
kryptt0n <vitaliksuhinin99@gmail.com>
2022-11-26 20:22:39 +0300

    ConsoleHelper recieved methods to work with console Connection became a wrapper for Socket class

--- src/ConsoleHelper.java
+++ src/ConsoleHelper.java
@@ -1,2 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String result = null;
+        while (true) {
+            try {
+                result = bufferedReader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return result;
+    }
+
+    public static int readInt() {
+        int result = 0;
+        while (true) {
+            try {
+                result = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return result;
+    }
 }
